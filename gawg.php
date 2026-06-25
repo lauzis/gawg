c89ac133a43178761a186a25e8ff348667fdf4f4
Aivars Lauzis <lauzis@inbox.lv>
2026-06-25 13:16:22 +0300

    feat: resolve issue #1

---
+++ gawg.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: GiveAway Winner Generator
+ * Description: Create giveaways, collect applicant entries, and run a lottery to pick winners.
+ * Version:     1.0.0
+ * Author:      Aivars Lauzis
+ * License:     GPL-2.0+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: gawg
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+define( 'GAWG_VERSION',    '1.0.0' );
+define( 'GAWG_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'GAWG_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+require_once GAWG_PLUGIN_DIR . 'includes/class-gawg.php';
+
+register_activation_hook( __FILE__, 'gawg_activate' );
+register_deactivation_hook( __FILE__, 'gawg_deactivate' );
+
+function gawg_activate() {}
+
+function gawg_deactivate() {}
